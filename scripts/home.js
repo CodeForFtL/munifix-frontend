02768c6cdc4d4e234db4e2c70c672a0a1b6b929f
dannysepler <dsepler@ufl.edu>
2014-07-31 16:12:51 -0400

    added geo page

--- scripts/home.js
+++ scripts/home.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	$('.choice').fadeIn('fast');
-	$('.choice').css('display', 'inline-block');
+	$('.fader').fadeIn('fast');
+	$('.fader').css('display', 'inline-block');
 
 	$('#one_geo').click(function() {
-		$('.choice').fadeOut('fast');
+		$('.fader').fadeOut('fast');
 	});
 
 	$('#one_remember').click(function() {
-		$('.choice').fadeOut('fast');
+		$('.fader').fadeOut('fast');
 	});
 });
